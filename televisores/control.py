5f02de798993a74df4fc11032cedbd450968702a
damazog <damazog@unal.edu.co>
2023-08-28 21:19:25 -0500

    errores de nombre nomás
    __

---
+++ televisores/control.py
@@ -0,0 +1,37 @@
+class Control:
+    def __init__(self):
+        self.tv
+    
+    def turnOn(self):
+        self.tv.turnON()
+    
+    def turnOff(self):
+         self.tv.turnOff()
+    
+    def canalUp(self):
+         self.tv.canalUp()
+    
+    def canalDown(self):
+         self.tv.canalDown()
+
+    def volumenUp(self):
+         self.tv.volumenUp()
+
+    def volumenDown(self):
+         self.tv.volumenDown()
+    
+    def setCanal(self, canal):
+         self.tv.setCanal(canal)
+
+    def setVolumen(self, volumen):
+         self.tv.setVolumen(volumen)
+
+    def enlazar(self, tv):
+        self.tv = tv
+        tv.setControl(self)
+    
+    def setTV(self, tv):
+        self.tv = tv
+
+    def getTV(self):
+        return self.tv
